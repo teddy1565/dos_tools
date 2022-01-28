fccfdc089daa9dd88d37840a6673d67cd641e9cf
teddy1565 <teddy1565@gmail.com>
2022-01-28 12:48:31 +0800

    no await promise, but it will process exit, so it is bad

---
+++ exec3.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+process.on("message",async (msg)=>{
+    let {target,loops,id} = JSON.parse(msg);
+    process.send(`[${id}]:on running`);
+    for (let i = 0;i<parseInt(loops);i++){
+        try {
+            axios.get(target);
+        } catch (error) {
+            console.log("Error");
+        }
+    }
+    process.send(`[${id}]:done`);
+    process.exit(0);
+})
